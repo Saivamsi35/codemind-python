dda8a9b8a8a322e1e971faf202648f570cdd9556
Sai Vamsi <109716189+Saivamsi35@users.noreply.github.com>
2022-10-27 00:02:02 +0530

    n = int(input()) nl = len(str(n)) sqr = n**2 last_no= sqr%10**nl if last_no == n:   print("Automorphic Number") else:   print("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,8 +1,8 @@
-num = int(input())
-n = len(str(num))
-sqr = num**2
-last = sqr%pow(10,n)
-if last == num:
+n = int(input())
+nl = len(str(n))
+sqr = n**2
+last_no= sqr%10**nl
+if last_no == n:
   print("Automorphic Number")
 else:
   print("Not an Automorphic Number")
