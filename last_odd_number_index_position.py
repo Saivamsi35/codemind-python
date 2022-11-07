c099ada6bbdf3ad0fbd936f50f74f8309f268354
Sai Vamsi <109716189+Saivamsi35@users.noreply.github.com>
2022-11-07 18:08:19 +0530

    #level 11 #Find the last odd element index position in a given array N=int(input()) lst=list(map(int,input().split())) for i in range(N,1,-1):     if lst[i-1]%2!=0:         print(i-1)         break

---
+++ last_odd_number_index_position.py
@@ -0,0 +1,9 @@
+#level 11
+#Find the last odd element index position in a given array
+N=int(input())
+lst=list(map(int,input().split()))
+for i in range(N,1,-1):
+    if lst[i-1]%2!=0:
+        print(i-1)
+        break
+        
